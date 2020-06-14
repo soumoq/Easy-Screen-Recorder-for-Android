53cfff9c0848b29220da1758df9a4c7aee5f5758
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-06-15 01:19:25 +0530

    add camera on floating layout (Camera class)

--- app/src/main/java/com/example/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.cameraapp;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.graphics.Matrix;
+import android.hardware.Camera;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -12,10 +15,13 @@ import android.util.Rational;
 import android.util.Size;
 import android.view.MotionEvent;
 import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +33,9 @@ import androidx.core.app.ActivityCompat;
 import androidx.lifecycle.LifecycleOwner;
 
 import org.jetbrains.annotations.Nullable;
+
+import java.io.IOException;
+import java.util.List;
 
 import io.hamed.floatinglayout.CallBack;
 import io.hamed.floatinglayout.FloatingLayout;
@@ -187,10 +196,16 @@ public class MainActivity extends AppCompatActivity implements CallBack {
     @Override
     public void onCreateListener(@Nullable View view) {
         Toast.makeText(this, "On Create", Toast.LENGTH_SHORT).show();
-        TextureView textureView123 = (TextureView) view.findViewById(R.id.view_finder123);
+        //TextureView textureView123 = (TextureView) view.findViewById(R.id.view_finder123);
 
         if (allPermissionsGranted()) {
-            startCamera(textureView123);
+            int camId= Camera.CameraInfo.CAMERA_FACING_FRONT;
+            //open camera
+            FrameLayout frameLayout = (FrameLayout) view.findViewById(R.id.collapsed_iv);
+            Camera camera = Camera.open(camId);
+
+            ShowCamera showCamera = new ShowCamera(this, camera);
+            frameLayout.addView(showCamera);
         }
 
         TextView textView = (TextView) view.findViewById(R.id.txtv);
@@ -202,4 +217,80 @@ public class MainActivity extends AppCompatActivity implements CallBack {
         Toast.makeText(this, "On Destroy", Toast.LENGTH_SHORT).show();
     }
 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+class ShowCamera extends SurfaceView implements SurfaceHolder.Callback {
+    private Camera camera;
+    private SurfaceHolder holder;
+
+
+    public ShowCamera(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+
+        Camera.Parameters parameters = camera.getParameters();
+
+        //change the orientation of the camera
+
+        List<Camera.Size> sizes=parameters.getSupportedPictureSizes();
+        Camera.Size mSize=null;
+
+        for (Camera.Size size: sizes)
+        {
+            mSize=size;
+        }
+
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE) {
+            parameters.set("orientation", "portrait");
+            camera.setDisplayOrientation(90);
+            parameters.setRotation(90);
+        } else {
+            parameters.set("orientation", "landscape");
+            camera.setDisplayOrientation(0);
+            parameters.setRotation(0);
+        }
+
+        parameters.setPictureSize(mSize.width,mSize.height);
+
+
+        camera.setParameters(parameters);
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        camera.stopPreview();
+        camera.release();
+    }
 }
