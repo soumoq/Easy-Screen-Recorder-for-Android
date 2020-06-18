1749b023317b2da2b2b656118c91e0d94740fd3d
soumojyotiganguly270@gmail.com <soumojyotiganguly270@gmail.com>
2020-06-18 19:44:43 +0530

    implement screen record in MainActivity

--- app/src/main/java/com/example/cameraapp/MainActivity.java
+++ app/src/main/java/com/example/cameraapp/MainActivity.java
@@ -2,18 +2,27 @@ package com.example.cameraapp;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Matrix;
 import android.hardware.Camera;
+import android.hardware.display.DisplayManager;
+import android.hardware.display.VirtualDisplay;
+import android.media.MediaRecorder;
+import android.media.projection.MediaProjection;
+import android.media.projection.MediaProjectionManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.Settings;
+import android.util.DisplayMetrics;
 import android.util.Rational;
 import android.util.Size;
+import android.util.SparseIntArray;
 import android.view.MotionEvent;
 import android.view.Surface;
 import android.view.SurfaceHolder;
@@ -23,10 +32,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.CameraX;
 import androidx.camera.core.Preview;
@@ -40,6 +52,8 @@ import com.google.android.material.snackbar.Snackbar;
 import org.jetbrains.annotations.Nullable;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import io.hamed.floatinglayout.CallBack;
@@ -49,6 +63,33 @@ import static android.widget.Toast.LENGTH_LONG;
 
 
 public class MainActivity extends AppCompatActivity implements CallBack {
+
+
+    private static final int REQUEST_CODE = 1000;
+    private static final int REQUEST_PERMISSION = 1001;
+    private static final SparseIntArray ORIENTATION = new SparseIntArray();
+
+    private MediaProjectionManager mMediaProjectionManager;
+    private MediaProjection mMediaProjection;
+    private VirtualDisplay mVirtualDisplay;
+    private MediaProjectionCallback mMediaProjectionCallback;
+    private MediaRecorder mMediaRecorder;
+
+    private int mScreenDensity;
+    private static final int DISPLAY_WIDTH = 720;
+    private static final int DISPLAY_HEIGHT = 1280;
+
+    static {
+        ORIENTATION.append(Surface.ROTATION_0, 90);
+        ORIENTATION.append(Surface.ROTATION_90, 0);
+        ORIENTATION.append(Surface.ROTATION_180, 270);
+        ORIENTATION.append(Surface.ROTATION_270, 180);
+    }
+
+    private RelativeLayout mRootLayout;
+    private ToggleButton mToggleButton;
+    private String mVideoUrl="";
+
 
     private static final int MY_PERMISSIONS_CAMERA = 1;
     private static final int CODE_DRAW_OVER_OTHER_APP_PERMISSION = 2084;
@@ -108,7 +149,215 @@ public class MainActivity extends AppCompatActivity implements CallBack {
             }
         });
 
+
+
+
+        //screen recode
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        mScreenDensity = displayMetrics.densityDpi;
+
+        mMediaRecorder = new MediaRecorder();
+        mMediaProjectionManager = (MediaProjectionManager) getSystemService(Context.MEDIA_PROJECTION_SERVICE);
+
+        mToggleButton = findViewById(R.id.toggleButton);
+        mRootLayout = findViewById(R.id.rootLayout);
+
+        mToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) +
+                        ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.RECORD_AUDIO)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) ||
+                            ActivityCompat.shouldShowRequestPermissionRationale(MainActivity.this, Manifest.permission.RECORD_AUDIO)) {
+                        mToggleButton.setChecked(false);
+                        Snackbar.make(mRootLayout, "Permission", Snackbar.LENGTH_INDEFINITE)
+                                .setAction("Enable", new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        ActivityCompat.requestPermissions(MainActivity.this,
+                                                new String[]{
+                                                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                        Manifest.permission.RECORD_AUDIO
+                                                },
+                                                REQUEST_PERMISSION);
+                                    }
+                                });
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this,
+                                new String[]{
+                                        Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                        Manifest.permission.RECORD_AUDIO
+                                },
+                                REQUEST_PERMISSION);
+                    }
+                } else {
+                    toggleScreenShare(v);
+                }
+            }
+        });
+
     }
+
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void toggleScreenShare(View v) {
+        ToggleButton toggleButton = (ToggleButton) v;
+        if (toggleButton.isChecked()) {
+            initRecorder();
+            reocrdScreen();
+        } else {
+            mMediaRecorder.stop();
+            mMediaRecorder.reset();
+            stopRecordScreen();
+
+            //mVideoView.setVisibility(View.VISIBLE);
+            //mVideoView.setVideoURI(Uri.parse(mVideoUrl));
+            //mVideoView.start();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void reocrdScreen() {
+        if (mMediaProjection == null) {
+            startActivityForResult(mMediaProjectionManager.createScreenCaptureIntent(), REQUEST_CODE);
+            return;
+        }
+
+        mVirtualDisplay = createVirtualDisplay();
+        mMediaRecorder.start();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private VirtualDisplay createVirtualDisplay() {
+        return mMediaProjection.createVirtualDisplay("Test", DISPLAY_WIDTH, DISPLAY_HEIGHT,
+                mScreenDensity, DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR, mMediaRecorder.getSurface(),
+                null, null);
+    }
+
+    private void initRecorder() {
+        try {
+            mMediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+            mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+
+            mVideoUrl = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) +
+                    new StringBuilder("/screen_record-").append(new SimpleDateFormat("dd-MM-yyy-hh_mm_ss")
+                            .format(new Date())).append(".mp4").toString();
+
+            mMediaRecorder.setOutputFile(mVideoUrl);
+            mMediaRecorder.setVideoSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+            mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+            mMediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mMediaRecorder.setVideoEncodingBitRate(512 * 1000);
+            mMediaRecorder.setVideoFrameRate(30);
+
+            int rotation = getWindowManager().getDefaultDisplay().getRotation();
+            int orientation = ORIENTATION.get(rotation + 90);
+            mMediaRecorder.setOrientationHint(orientation);
+            mMediaRecorder.prepare();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode != REQUEST_CODE) {
+            Toast.makeText(this, "Unk error", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (resultCode != RESULT_OK) {
+            Toast.makeText(this, "Permission denied", Toast.LENGTH_SHORT).show();
+            mToggleButton.setChecked(false);
+            return;
+        }
+
+        mMediaProjectionCallback = new MediaProjectionCallback();
+        mMediaProjection = mMediaProjectionManager.getMediaProjection(resultCode, data);
+        mMediaProjection.registerCallback(mMediaProjectionCallback, null);
+        mVirtualDisplay = createVirtualDisplay();
+        mMediaRecorder.start();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private class MediaProjectionCallback extends MediaProjection.Callback {
+
+        @Override
+        public void onStop() {
+            super.onStop();
+
+            if (mToggleButton.isChecked()) {
+                mToggleButton.setChecked(false);
+                mMediaRecorder.stop();
+                mMediaRecorder.reset();
+            }
+
+            mMediaProjection = null;
+            stopRecordScreen();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void stopRecordScreen() {
+        if (mVirtualDisplay == null)
+            return;
+
+        mVirtualDisplay.release();
+        destroyMediaProjection();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    private void destroyMediaProjection() {
+        if (mMediaProjection != null) {
+            mMediaProjection.unregisterCallback(mMediaProjectionCallback);
+            mMediaProjection.stop();
+            mMediaProjection = null;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_PERMISSION: {
+                if (grantResults.length > 0 && (grantResults[0] + grantResults[1] == PackageManager.PERMISSION_GRANTED)) {
+                    toggleScreenShare(mToggleButton);
+                } else {
+                    mToggleButton.setChecked(false);
+                    Snackbar.make(mRootLayout, "Permission", Snackbar.LENGTH_INDEFINITE)
+                            .setAction("Enable", new View.OnClickListener() {
+                                @Override
+                                public void onClick(View v) {
+                                    ActivityCompat.requestPermissions(MainActivity.this,
+                                            new String[]{
+                                                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                    Manifest.permission.RECORD_AUDIO
+                                            },
+                                            REQUEST_PERMISSION);
+                                }
+                            });
+                }
+                return;
+            }
+        }
+    }
+
+
+
+
+
 
 
     @SuppressLint("RestrictedApi")
---
+++ app/src/main/java/com/example/cameraapp/ScreenRecoder.java
@@ -0,0 +1,7 @@
+package com.example.cameraapp;
+
+public class ScreenRecoder {
+
+
+
+}
